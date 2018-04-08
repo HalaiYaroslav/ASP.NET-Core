54050a6ce23c4340e9e14a76bb1f45cae1af8fc1
Yaroslav Halai <galai.yaroslaw@gmail.com>
2018-04-08 22:37:37 +0300

    WorkWithVS Test Project

---
+++ WorkWithVS/WorkWithVS.Tests/HomeControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkWithVS.Controllers;
+using WorkWithVS.Models;
+using Xunit;
+
+namespace WorkWithVS.Tests
+{
+    public class HomeControllerTests
+    {
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
+        {
+            //Arrange
+            var mock = new Mock<IRepository>(); //Creation of moq object and interface definition which must be realized
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController { Repository = mock.Object };
+            //Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            //Assert
+            Assert.Equal(controller.Repository.Products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
+                && p1.Price == p2.Price));
+        }
+
+        #region Test for checking next problem: component sending duplicate requests to storage (many requests to Data Base)      
+        [Fact]
+        public void RepositoryPropertyCalledOnce()
+        {
+            //Arrange
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(new[] { new Product { Name = "P1", Price = 100 } });
+            var controller = new HomeController { Repository = mock.Object };
+            //Act
+            var result = controller.Index();
+            //Assert
+            mock.VerifyGet(m => m.Products, Times.Once);
+        }
+        #endregion
+    }
+}
---
+++ WorkWithVS/WorkWithVS/Models/SimpleRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkWithVS.Models
+{
+    public class SimpleRepository : IRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product{ Name = "P1", Price= 275M},
+                new Product{ Name = "P2", Price= 48.95M},
+                new Product{ Name = "P3", Price= 19.50M},
+                new Product{ Name = "P4", Price= 34.95M},
+            };
+
+            foreach (var p in initialItems)
+            {
+                AddProduct(p);
+            }            
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+    }
+}
