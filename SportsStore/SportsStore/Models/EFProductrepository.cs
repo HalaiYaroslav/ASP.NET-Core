499626c7c39abeb5f7bc256e20723393a5d60b55
Yaroslav Halai <galai.yaroslaw@gmail.com>
2018-04-13 21:58:28 +0300

    Removed DB files from RA. Some changes in SportsStore

---
+++ SportsStore/SportsStore/Models/EFProductrepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductrepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductrepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<Product> Products => context.Products;
+    }
+}
--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -5,18 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
+        IConfigurationRoot Configuration;        
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json").Build(); //adding configuration settings from file appsettings.json and store it in the Configuration property
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IProductRepository, EFProductrepository>();
             services.AddMvc();
         }
 
@@ -32,6 +45,8 @@ namespace SportsStore
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
+
+            SeedData.EnusrePopulated(app);
         }
     }
 }
